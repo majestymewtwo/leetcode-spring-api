7bb3962ad138c06205f5ff9e9b62b36fc9f9d833
majestymewtwo <majestymewtwo@gmail.com>
2024-02-27 14:17:21 +0530

    initial commit

---
+++ src/main/java/com/example/leetcodespringapi/LeetcodeSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetcodespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeSpringApiApplication.class, args);
+    }
+
+}
